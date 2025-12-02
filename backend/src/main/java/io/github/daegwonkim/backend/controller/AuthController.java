3b4aa86945cc36257f200e592b79ae01ef36c438
kimdaegwon <daegwonkim.dev@gmail.com>
2025-12-02 17:30:57 +0900

    feat: 인증번호 전송/검증을 위한 로직 추가

--- backend/src/main/java/io/github/daegwonkim/backend/controller/AuthController.java
+++ backend/src/main/java/io/github/daegwonkim/backend/controller/AuthController.java
@@ -1,7 +1,10 @@
 package io.github.daegwonkim.backend.controller;
 
 import io.github.daegwonkim.backend.dto.auth.request.*;
+import io.github.daegwonkim.backend.service.VerificationCodeService;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,47 +12,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
-    @Operation(
-            summary = "본인인증",
-            description = "이름, 생년월일, 성별, 휴대폰 정보를 통해 본인인증을 수행합니다."
-    )
-    @PostMapping("/identity/verify")
-    public void identityVerification(@RequestBody IdentityVerificationRequest request) {}
+    private final VerificationCodeService verificationCodeService;
 
     @Operation(
             summary = "인증번호 전송",
             description = "사용자에게 인증번호를 전송합니다."
     )
     @PostMapping("/verification-code/send")
-    public void verificationCodeSend(@RequestBody VerificationCodeSendRequest request) {}
+    public void sendVerificationCode(@Valid @RequestBody VerificationCodeSendRequest request) {
+        verificationCodeService.sendVerificationCode(request.phoneNo());
+    }
 
     @Operation(
             summary = "인증번호 검증",
             description = "사용자가 입력한 인증번호를 검증합니다."
     )
-    @PostMapping("/verification-code/verify")
-    public void codeVerification(@RequestBody CodeVerificationRequest request) {}
-
-    @Operation(
-            summary = "휴대폰 번호 검증",
-            description = "기존 계정에 존재하는 휴대폰 번호인지 검증합니다."
-    )
-    @PostMapping("/phone-number/verify")
-    public void signin(@RequestBody PhoneNumberVerificationRequest request) {}
-
-    @Operation(
-            summary = "회원가입",
-            description = "사용자의 새로운 계정을 생성합니다."
-    )
-    @PostMapping("/signup")
-    public void signup(@RequestBody UserSignupRequest request) {}
-
-    @Operation(
-            summary = "로그인",
-            description = "기존 계정으로 로그인합니다."
-    )
-    @PostMapping("/signin")
-    public void signin(@RequestBody UserSigninRequest request) {}
+    @PostMapping("/verification-code/confirm")
+    public void confirmVerificationCode(@Valid @RequestBody VerificationCodeConfirmRequest request) {
+        verificationCodeService.confirmVerificationCode(request.phoneNo(), request.verificationCode());
+    }
 }
