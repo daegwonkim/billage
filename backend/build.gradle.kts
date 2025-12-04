d5a3adf8ff9449def86f7e738df06550ecfb77d0
kimdaegwon <daegwonkim.dev@gmail.com>
2025-12-04 14:29:16 +0900

    fix: PostgreSQL 의존성 추가

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.postgresql:postgresql")
 
     implementation("net.nurigo:sdk:4.3.0")
 
