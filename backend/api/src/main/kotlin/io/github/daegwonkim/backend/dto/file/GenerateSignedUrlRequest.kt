c81d859c799cd47d36550e162d63e31c65b38fb5
kimdaegwon <daegwonkim.dev@gmail.com>
2025-12-18 15:26:00 +0900

    fix: 코드 구조 변경

--- backend/api/src/main/kotlin/io/github/daegwonkim/backend/dto/file/GenerateSignedUrlRequest.kt
+++
@@ -1,6 +0,0 @@
-package io.github.daegwonkim.backend.dto.file
-
-data class GenerateSignedUrlRequest(
-    val bucket: String,
-    val fileName: String
-)
--- backend/api/src/main/kotlin/io/github/daegwonkim/backend/dto/file/GenerateSignedUrlResponse.kt
+++
@@ -1,7 +0,0 @@
-package io.github.daegwonkim.backend.dto.file
-
-data class GenerateSignedUrlResponse(
-    val fileKey: String,
-    val uploadUrl: String,
-    val publicUrl: String
-)
