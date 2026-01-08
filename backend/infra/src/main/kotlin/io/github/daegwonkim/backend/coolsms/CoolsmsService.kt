a10e8ad13cd99b8543c2929bff274e3484533993
kimdaegwon <daegwonkim.dev@gmail.com>
2026-01-08 13:10:26 +0900

    fix: CoolsmsService 관련 함수 리팩토링

--- backend/infra/src/main/kotlin/io/github/daegwonkim/backend/coolsms/CoolsmsService.kt
+++ backend/infra/src/main/kotlin/io/github/daegwonkim/backend/coolsms/CoolsmsService.kt
@@ -4,6 +4,7 @@ import io.github.daegwonkim.backend.exception.infra.ExternalApiException
 import io.github.daegwonkim.backend.log.logger
 import net.nurigo.sdk.message.model.Message
 import net.nurigo.sdk.message.request.SingleMessageSendingRequest
+import net.nurigo.sdk.message.response.SingleMessageSentResponse
 import net.nurigo.sdk.message.service.DefaultMessageService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
@@ -18,30 +19,38 @@ class CoolsmsService(
         private const val SUCCESS_CODE = "2000"
     }
 
-    fun sendSms(phoneNo: String, message: String): SendSmsResponse {
-        val smsMessage = Message().apply {
-            from = smsFrom
-            to = phoneNo
-            text = message
-        }
+    fun sendSms(to: String, text: String): SendSmsResponse {
+        val message = createMessage(to, text)
 
         try {
-            val response = messageService.sendOne(SingleMessageSendingRequest(smsMessage))
-                ?: throw ExternalApiException(apiName = "CoolSMS")
-
-            if (response.statusCode != SUCCESS_CODE) {
-                throw ExternalApiException(
-                    apiName = "CoolSMS",
-                    statusCode = response.statusCode.toIntOrNull()
-                )
-            }
-
+            val response = requestSendOne(message)
             return SendSmsResponse(response.statusCode, response.statusMessage)
         } catch (e: ExternalApiException) {
             throw e
         } catch (e: Exception) {
-            logger.error(e) { "CoolSMS API 호출 중 예외 발생: phoneNo=****${phoneNo.takeLast(4)}" }
+            logger.error(e) { "CoolSMS API 호출 중 예외 발생: phoneNo=****${to.takeLast(4)}" }
             throw ExternalApiException(apiName = "CoolSMS", cause = e)
         }
+    }
+
+    private fun createMessage(to: String, text: String) =
+        Message().apply {
+            from = smsFrom
+            this.to = to
+            this.text = text
+        }
+
+    private fun requestSendOne(message: Message): SingleMessageSentResponse {
+        val response = messageService.sendOne(SingleMessageSendingRequest(message))
+            ?: throw ExternalApiException(apiName = "CoolSMS")
+
+        if (response.statusCode != SUCCESS_CODE) {
+            throw ExternalApiException(
+                apiName = "CoolSMS",
+                statusCode = response.statusCode.toIntOrNull()
+            )
+        }
+
+        return response
     }
 }
